b66b802527e46b9c2e55683066fe0a24c34256fe
jailan-sh <jilan.elsherbiny@gmail.com>
2024-06-29 13:28:15 +0300

    do utility file

--- task-2/utilitis.js
+++
@@ -1,9 +0,0 @@
-// object for all helper functions needed to validate data and handling error
-
-// function to run if file not exise or empty render something in html (file not found or data not found )
-
-// function to check objects present and each property not undefined
-
-// function to render data create element .html and append child
-
-//
---
+++ task-2/utility.js
@@ -0,0 +1,17 @@
+// object for all helper functions needed to validate data and handling error
+
+// function to run if file not exise or empty render something in html (file not found or data not found )
+const fileError = function (responseStatus) {
+  const p = document.createElement("p");
+  p.innerText = `Page does not exist (${responseStatus})`;
+  document.body.appendChild(p);
+};
+
+// function to check objects present and each property not undefined:
+const validateCarObject = function (obj) {
+  // properties: image, title , start_production, class
+};
+
+// function to render data create element .html and append child(table)
+
+//
