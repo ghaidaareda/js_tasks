9dbbca6a714aced53ce80e30efe25d2542417e8c
ghaidaareda <dodo.sa7afy@gmail.com>
2024-07-06 00:48:44 +0300

    errors

--- task-2/script.js
+++ task-2/script.js
@@ -1,5 +1,8 @@
 fetch("./noblists.json")
 .then(function(response) {
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
     return response.json();
 })
 .then(function(noblists) {
@@ -11,7 +14,7 @@ fetch("./noblists.json")
             <td><img src='${item.image}'></td>
             <td>${item.name}</td>
             <td>${item.year}</td>
-            <td>${item.content}</td>
+            <td>${item.country}</td>
             <td>${item.category}</td>
             <td>${item.rationale}</td>
 
@@ -22,3 +25,6 @@ fetch("./noblists.json")
     placeholder.innerHTML = output;
 
 })
+.catch(function(error) {
+    console.error('There was a problem with the fetch operation:', error);
+});
